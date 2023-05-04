913e4d56827af8bb84898352a5f8803939a36b46
Omar Zahid <omarzahid@outlook.com>
2023-05-04 14:50:09 +0800

    Created project service

---
+++ src/main/java/com/magnuscode/project/ProjectService.java
@@ -0,0 +1,65 @@
+package com.magnuscode.project;
+
+import java.util.List;
+
+import org.hibernate.ObjectNotFoundException;
+
+import com.magnuscode.task.Task;
+import com.magnuscode.user.UserService;
+
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.security.UnauthorizedException;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class ProjectService {
+
+    private final UserService userService;
+
+    @Inject
+    public ProjectService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Uni<Project> findById(long id) {
+        return userService.getCurrentUser()
+                .chain(user -> Project.<Project>findById(id)
+                        .onItem().ifNull().failWith(() -> new ObjectNotFoundException(id, "Project"))
+                        .onItem().invoke(project -> {
+                            if (!user.equals(project.user)) {
+                                throw new UnauthorizedException("You are not allowed to update this project");
+                            }
+                        }));
+    }
+
+    public Uni<List<Project>> listForUser() {
+        return userService.getCurrentUser()
+                .chain(user -> Project.find("user", user).list());
+    }
+
+    @WithTransaction
+    public Uni<Project> create(Project project) {
+        return userService.getCurrentUser()
+                .chain(user -> {
+                    project.user = user;
+                    return project.persistAndFlush();
+                });
+    }
+
+    @WithTransaction
+    public Uni<Project> update(Project project) {
+        return findById(project.id)
+                .chain(p -> Project.getSession())
+                .chain(s -> s.merge(project));
+    }
+
+    @WithTransaction
+    public Uni<Void> delete(long id) {
+        return findById(id)
+                .chain(p -> Task.update("project = null where project = ?1", p)
+                        .chain(i -> p.delete()));
+    }
+
+}
